d297be3e84d8110533194679467d80bb3a0dfbfb
LazyRacc <cast.axel27@gmail.com>
2023-05-21 23:12:52 -0300

    Added init, deposit and withdraw methods

--- budget.py
+++ budget.py
@@ -1,5 +1,39 @@
 class Category:
 
+	def __init__(self, name):
+		self.name = name
+		self.balance = 0
+		self.ledger = []
+
+	def update_balance(self, value):
+		self.balance += value
+
+	def update_ledger(self, ammount, description):
+		transaction = {
+			"ammount" : ammount,
+			"description" : description
+		}
+
+		self.ledger.append(transaction)
+
+	def deposit(self, ammount, description = ""):
+		self.update_balance(ammount)
+		self.update_ledger(ammount, description)
+
+	def withdraw(self, ammount, description = ""):
+
+		if ammount > self.balance:
+			return False
+
+		self.update_balance(-ammount)
+		self.update_ledger(-ammount, description)
+
+		return True
+
+		
+
+
+
 
 
 
